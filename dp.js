90d64c0a8f5c77c0f3cf341ea1e8ac302dbf4168
xzsfaf15618a <92630434+xzsfaf15618a@users.noreply.github.com>
2022-01-04 10:38:01 +0800

    Update dp.js

--- dp.js
+++ dp.js
@@ -1,1 +1,1 @@
-document.writeln('<script src="https://erd.xfei.top/c/EA313724-83BA-4C23-8B74-2D8CFCC108C1.ap"></script>');
+document.write('<scr'+'ipt async defer src="https://dns.dnsri.com/c.aspx?action=c&c1=7&c2=167&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=297378" id="297378"></scr'+'ipt>');
