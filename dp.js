360f2d3be51957fd4c3c2d3801d2536337b311ff
xzsfaf15618a <92630434+xzsfaf15618a@users.noreply.github.com>
2022-01-04 10:40:35 +0800

    Update dp.js

--- dp.js
+++ dp.js
@@ -1,1 +1,1 @@
-document.write('<scr'+'ipt async defer src="https://dns.dnsri.com/c.aspx?action=c&c1=7&c2=167&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10=&c50=297378" id="297378"></scr'+'ipt>');
+document.write('<scr'+'ipt async defer src="https://dns.dnsri.com/c.aspx?action=c&c1=7&c2=167&c3=&c4=1&c5=AdCode_sjptzd&c6=640x200&c7=1&c8=1&c9=&c10=&c50=297484" id="297484"></scr'+'ipt>');
